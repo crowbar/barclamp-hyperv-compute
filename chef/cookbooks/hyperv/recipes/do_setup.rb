428c022d6d6bdc4a033e734e9829b0c261464da4
Vincent Untz <vuntz@suse.com>
2015-09-09 11:58:01 +0200

    Add note that this got moved to merged repo

--- chef/cookbooks/hyperv/recipes/do_setup.rb
+++
@@ -1,7 +0,0 @@
-raise if not node[:platform] == 'windows'
-
-include_recipe "hyperv::windows_features"
-include_recipe "hyperv::setup_networking"
-include_recipe "hyperv::7zip"
-include_recipe "hyperv::python"
-include_recipe "hyperv::python_archive"
